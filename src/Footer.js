7904771a3a173423da26ebf5e98d1a0959a60aa4
Lewis Broadhurst <lewis1broadhurst@gmail.com>
2022-06-27 19:55:37 +0100

    Footer CSS added

--- src/Footer.js
+++ src/Footer.js
@@ -1,39 +1,41 @@
-
+import "./Footer.css";
 
 const Footer = () => {
   return (
     <>
-        <section className="Newsletter-Footer">
-            <h3>Newsletter</h3>
-            <p>Sign up to be informed about our work and how you can help, such as fundraising, campaigning and events. You can unsubscribe at any time.</p>
-            
-            <form>
-                <input type="text"></input>
-                <button type="submit">Subscribe</button>
-            </form>
-        </section>
+        <footer className="generic-footer">
 
-        <section className="SocialMedia-Footer">
-            <h3>Social Media</h3>
-            <ul>
-                <li>Twitter</li>
-                <li>Instagram</li>
-                <li>Facebook</li>
-                <li>Snapchat</li>
-                <li>TikTok</li>
-            </ul>
-        </section>
+            <section className="Newsletter-Footer">
+                <h3>Newsletter</h3>
+                <p>Sign up to be informed about our work and how you can help, such as fundraising, campaigning and events. You can unsubscribe at any time.</p>
+                
+                <form className="subscribe-footer-form">
+                    <input type="email" placeholder="Enter your email here"></input>
+                    <button type="submit">Subscribe</button>
+                </form>
+            </section>
 
-        <section className="Legal-Footer">
-            <h3>Legal</h3>
-            <ul>
-                <li>Terms and Conditions</li>
-                <li>Cookies</li>
-                <li>Privacy Notice</li>
-            </ul>
-        </section>
+            <section className="SocialMedia-Footer">
+                <h3>Social Media</h3>
+                <ul>
+                    <li>Twitter</li>
+                    <li>Instagram</li>
+                    <li>Facebook</li>
+                    <li>Snapchat</li>
+                    <li>TikTok</li>
+                </ul>
+            </section>
 
-        
+            <section className="Legal-Footer">
+                <h3>Legal</h3>
+                <ul>
+                    <li>Terms and Conditions</li>
+                    <li>Cookies</li>
+                    <li>Privacy Notice</li>
+                </ul>
+            </section>
+
+        </footer>
         
     </>
   )
