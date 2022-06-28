37ec849b3408a9e955c598aa2e567c390a9a6c6a
Lewis Broadhurst <lewis1broadhurst@gmail.com>
2022-06-28 10:31:08 +0100

    Header Responsiveness Done

--- src/Header/Header.js
+++ src/Header/Header.js
@@ -3,16 +3,31 @@ import { faBars } from "@fortawesome/free-solid-svg-icons";
 import PawLogo from "./Images/white-paw-print-transparent-background-17.png";
 
 import "./Header_Stylesheets/HeaderBasics.css";
+import "./Header_Stylesheets/HeaderTablet.css";
 
 const Header = () => {
   return (
     <>
         <header className="Header-Mobile">
+           
             <img src={PawLogo} alt="Adopt A Paw Logo" className="Logo-Header"/>
-
+            
             <h1 className="AAP-Header-Title">Adopt A Paw</h1>
 
+            <nav className="Expanded-Navbar-Header">
+              <ul>
+                <li>Adopt</li>
+                <li>Animals</li>
+                <li>Organisations</li>
+                <li>Success Stories</li>
+                <li>News</li>
+              </ul>
+            </nav>
+
             <span className="DropDown-Button-Header"><FontAwesomeIcon icon={faBars} /></span>
+
+            <span className="Account-Link">Account</span>
+
         </header>
     </>
   )
