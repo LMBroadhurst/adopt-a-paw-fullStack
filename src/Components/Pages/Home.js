e0bd43f3d4b8018488eda0ae13662f24617c302d
Kieran Sandford <kieran.sandford1@gmail.com>
2022-06-28 11:24:04 +0100

    added in display_carousel to home

--- src/Components/Pages/Home.js
+++ src/Components/Pages/Home.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import './Home.css';
+import Display_Carousel from '../../Homepage_Carousel/Display_Carousel';
 
 function Home() {
   return (
@@ -7,6 +8,9 @@ function Home() {
         <div className='homepage-image'>
             <img src={process.env.PUBLIC_URL + '/familydog.jpeg'}/>
             <div className='centred'>Say Hello to Your New Best Friend</div>
+        </div>
+        <div>
+        <Display_Carousel />
         </div>
     </div>
   )
