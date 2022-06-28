11f48e3c2e04f8f577084dd10edf5846483cc636
Rebecca Bartholomew <cerulean.rebecca20@hotmail.com>
2022-06-28 10:17:22 +0100

    created animal service

--- backend/src/main/java/com/example/demo/services/AnimalService.java
+++ backend/src/main/java/com/example/demo/services/AnimalService.java
@@ -1,4 +1,15 @@
 package com.example.demo.services;
 
+
+import com.example.demo.repositories.AnimalRepo;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
 public class AnimalService {
+
+    @Autowired
+    private AnimalRepo animalRepo;
+
+    
 }
