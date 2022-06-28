12d388d397206fedabfb2b9c3f9112976bb9f662
Kieran Sandford <kieran.sandford1@gmail.com>
2022-06-28 11:11:44 +0100

    carousel displaying images

--- src/App.js
+++ src/App.js
@@ -1,13 +1,18 @@
+import "./App.css"
 import Footer from "./Footer/Footer";
 import Header from "./Header/Header";
+import Homepage_Carousel, {CarouselItem} from "./Homepage_Carousel/Homepage_Carousel"
+import Display_Carousel from "./Homepage_Carousel/Display_Carousel";
 
 function App() {
   return (
     <>
       <Header />
+      <Display_Carousel />
       <Footer />
     </>
   );
 }
 
 export default App;
+
