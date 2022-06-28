d7f6765e082f2e1e357fae4ba27feb510eaa6e4c
Lewis Broadhurst <lewis1broadhurst@gmail.com>
2022-06-28 20:24:20 +0100

    Org Animations

--- src/OrganisationsPage/OrganisationFilter/OrganisationFilter.js
+++ src/OrganisationsPage/OrganisationFilter/OrganisationFilter.js
@@ -2,6 +2,13 @@ import "./OrgansFiltBasic.css";
 import "./OrgansFiltTablet+.css";
 
 const OrganisationFilter = () => {
+
+    const toggleStyle = () => {
+        const input = document.getElementsByClassName("Filt-Input-Org");
+
+       input.style.backgroundColor = "blue";
+    }
+
   return (
     <>
         <section className="Org-Filter-Bar">
@@ -11,12 +18,12 @@ const OrganisationFilter = () => {
             <form className="Org-Filter-Form">
 
                 <div className="Name-Check-Label">
-                    <input type="checkbox" className="Filt-Checkbox-Org"/>
+                    <input type="text" className="Filt-Input-Org" onChange={toggleStyle} />
                     <label htmlFor="Org-Name-Filt">Name</label>
                 </div>
                 
                 <div className="Location-Check-Label">
-                    <input type="checkbox" className="Filt-Checkbox-Org" />
+                    <input type="text" className="Filt-Input-Org" onChange={toggleStyle} />
                     <label>Location</label>
                 </div>
                 
