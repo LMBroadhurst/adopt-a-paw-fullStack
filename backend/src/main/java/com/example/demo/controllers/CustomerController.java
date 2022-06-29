f782256991450a7e9e5438be5603596bbeb7c488
Cem Gurhan <cemg@live.co.uk>
2022-06-29 10:49:07 +0100

    fixed a problem

--- backend/src/main/java/com/example/demo/controllers/CustomerController.java
+++ backend/src/main/java/com/example/demo/controllers/CustomerController.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-@RequestMapping("/customer")
+//@RequestMapping("/customer")
 @CrossOrigin(origins = "http://localhost:3000")
 public class CustomerController {
 
